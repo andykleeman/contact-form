afbb375b00a61a694e592082df1b20a06e79e6fe
Andy Kleeman <andykleeman@gmail.com>
2013-04-04 15:59:03 +0100

    Error functiona and renaming
    - updated success / error function
    - commented out windows server setting
    - renamed contact_data contact_validation to be more obvious

--- inc/contact_form.php
+++ inc/contact_form.php
@@ -14,7 +14,7 @@ else
 
 function processForm()
 {
-    include('contact_data.php');
+    include('contact_validation.php');
 
     if($count > 0)
     {
@@ -26,6 +26,7 @@ function processForm()
    
     
     $emailMesage = new MDS_EmailSender_Message();
+    $success = true;
     
     try
     {
@@ -44,20 +45,33 @@ function processForm()
         Enquiry: <strong>'.$enquiry.'</strong><br />');
 
         $emailSender = new MDS_EmailSender();
-        $emailSender->setWinMailServer();
+    /* 	
+    //	use this setting on windows servers
+      	$emailSender->setWinMailServer();
+    */
         $emailSender->send($emailMesage);
     }
     catch (MDS_EmailSender_Exception $e)
     {
+        echo 'Error Code is : ' . $e->errorCode();
+        echo 'Error Message is : ' . $e->errorMessage();
+        // You can find a list of the error codes at the bottom of the file in the Exception Class.
         $success = false;
     }
     
     
-    // send Json Response - this is the returned thanks message and also fades out the form
-    $response = array(
-          'ok' => true,
-          'msg' => "<div class='success'>Thank you for your enquiry we will endever to get back<br /> to you soon as possible.<br /><br /></div><script type='text/javascript'>jQuery('#contact-form').fadeOut(300); jQuery('#msg').css('float', 'none');</script>");
-   
+    if ($success === true) {
+    	// SUCCESS RESPONSE
+	    // send Json Response - this is the returned thanks message and also fades out the form
+	    $response = array(
+	          'ok' => true,
+	          'msg' => "<div class='success'>Thank you for your enquiry we will endever to get back<br /> to you soon as possible.<br /><br /></div><script type='text/javascript'>jQuery('#contact-form').fadeOut(300); jQuery('#msg').css('float', 'none');</script>");
+	} else {
+		    // FAILED
+	    $response = array(
+	          'ok' => true,
+	          'msg' => "Sorry there was an error and your message has not been sent.");	
+	}
     
     return $response;
 }
